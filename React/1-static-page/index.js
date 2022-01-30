111682aa99bc2ef079bd7093c7742c83b4eb4dc3
Emily Richardson <richardson96@gmail.com>
2022-01-29 23:39:19 -0600

    static page using JSX

---
+++ React/1-static-page/index.js
@@ -0,0 +1,15 @@
+const page = (
+    <div>
+        <img src="./react-logo.png" width="50px"></img>
+        <h1>Fun Facts About React</h1>
+        <ul>
+            <li>First released in 2013</li>
+            <li>Originally created by Jordan Walke</li>
+            <li>Has 100K+ stars on Github</li>
+            <li>Maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
+)
+
+ReactDOM.render(page, document.getElementById("root"));
