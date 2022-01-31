6d55003eb198e09495360e6a438b717c685c1266
Emily Richardson <richardson96@gmail.com>
2022-01-30 20:18:37 -0600

    separated components, added imports to index

--- React/1-static-page/index.js
+++ React/1-static-page/index.js
@@ -1,44 +1,10 @@
-function Header() {
-    return (
-        <header className="header">
-            <nav className="nav">
-                <img className="nav-logo" src="./react-logo.png"></img>
-                <ul className="nav-items">
-                    <li>Pricing</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-        </header>
+import React from "react"
+import ReactDOM from "react-dom"
+import Header from "./Header"
+import Footer from "./Footer"
+import MainContent from "./MainContent"
 
-    )
-}
-
-function MainContent() {
-    return (
-        <div>
-        <h1>Fun Facts About React</h1>
-            <ol>
-                <li>First released in 2013</li>
-                <li>Originally created by Jordan Walke</li>
-                <li>Has 100K+ stars on Github</li>
-                <li>Maintained by Facebook</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ol>
-        </div>
-    )
-}
-
-function Footer() {
-    return (
-        <footer className="footer">
-            <small>© 2022 Richardson development. All rights reserved.</small>
-        </footer>
-    )
-}
-
-
-function Page() { 
+function Page() {
     return (
         <div>
             <Header />
@@ -48,6 +14,4 @@ function Page() {
     )
 }
 
-
-
-ReactDOM.render(<Page />, document.getElementById("root"));
+ReactDOM.render(<Page />, document.getElementById("root"))
