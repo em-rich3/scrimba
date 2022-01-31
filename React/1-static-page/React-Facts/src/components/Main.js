34dc1b69ac103475775ee2215d6aa8e8ad05e870
Emily Richardson <richardson96@gmail.com>
2022-01-30 21:43:57 -0600

    created React Facts static page

---
+++ React/1-static-page/React-Facts/src/components/Main.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Main() {
+    return (
+        <main>
+            <h1 className="main--title">Fun Facts about React</h1>
+            <ul className="main--facts">
+                <li>First released in 2013</li>
+                <li>Originally created by Jordan Walke</li>
+                <li>Has 100K+ stars on GitHub</li>
+                <li>Maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
