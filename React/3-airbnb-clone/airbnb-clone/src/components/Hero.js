bf640ed3df791fee42b739aa7aa5b002ec9854e6
em-rich3 <richardson96@gmail.com>
2022-03-17 17:06:45 -0500

    completed airbnb clone

---
+++ React/3-airbnb-clone/airbnb-clone/src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Main() {
+    return (
+        <section className="hero">
+            <img src="images/photo-grid.png" className="hero--image" alt="clear"/>
+            <h1 className="hero--title">Online Experiences</h1>
+            <p className="hero--text">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+            </p>
+          
+        </section>
+    )
+}
