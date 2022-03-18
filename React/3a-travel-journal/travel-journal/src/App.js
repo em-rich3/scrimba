fdb4092dbab3b33f19feeb04e884f77a72e6e880
em-rich3 <richardson96@gmail.com>
2022-03-18 11:06:28 -0500

    completed header

---
+++ React/3a-travel-journal/travel-journal/src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "./components/Header"
+import Card from "./components/Card"
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Card />
+    </div>
+    
+  )
+}
---
+++ React/3a-travel-journal/travel-journal/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <div className="header">
+           <img className="header--logo" src="images/globe.svg" alt="earth icon"/>
+        <h1 className="header--title">my travel journal</h1> 
+        </div>
+        
+    )
+}
