f924759d5ce5f97fcbd745c9b2e6422c14a1eb5a
em-rich3 <richardson96@gmail.com>
2022-03-18 16:28:45 -0500

    form setup

--- React/4-meme-generator/meme-generator/src/App.js
+++ React/4-meme-generator/meme-generator/src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Header from "../src/components/Header"
+import Meme from "../src/components/Meme"
 
 
 export default function App() {
     return (
       <div>
         <Header />
+        <Meme />
       </div>
     )
     
