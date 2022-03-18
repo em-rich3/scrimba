9ed88db213c63f2d8629fb2ab264aac8760d54ee
em-rich3 <richardson96@gmail.com>
2022-03-18 13:22:38 -0500

    completed header

---
+++ React/4-meme-generator/meme-generator/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <div className="header">
+            <img src="images/troll-face.png" className="header--image" alt="meme face"/>
+            <h1 className="header--title">Meme Generator</h1>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </div>
+        
+    )
+}
