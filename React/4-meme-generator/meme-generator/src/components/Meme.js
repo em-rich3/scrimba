a75f33cd4cc78310b7785ee92887d92a53477f1c
em-rich3 <richardson96@gmail.com>
2022-03-18 16:47:11 -0500

    created random URL selection function from dataset

--- React/4-meme-generator/meme-generator/src/components/Meme.js
+++ React/4-meme-generator/meme-generator/src/components/Meme.js
@@ -1,10 +1,18 @@
 import React from "react"
+import memesData from "../memesData"
 
 export default function Meme() {
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url;
+        console.log(url);
+    }
+
     return (
         <main>
-
-            <form className="form">
+            <div className="form">
                 <input 
                     type="text"
                     placeholder="Top text"
@@ -15,11 +23,14 @@ export default function Meme() {
                     placeholder="Bottom text"
                     className="form--input"
                 />
-                <button className="form--button">Get a New Meme Image 🖼</button>
-            </form> 
-
+                <button 
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼
+                </button>
+            </div>
         </main>
-       
     )
     
 }
